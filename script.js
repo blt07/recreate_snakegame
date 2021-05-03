d6589c0016da8d7dae37207fdd648d8ae62bc8a9
Paulo Lima <eng.paulocosta@icloud.com>
2021-05-03 13:22:54 -0300

    Finished the body snake and the form to creat the food.

--- script.js
+++ script.js
@@ -7,7 +7,17 @@ snake[0] = {
     y: 8* box
 }
 
+let food = {
+    x: Math.floor(Math.random() * 15 + 1)* box,
+    y: Math.floor(Math.random() * 15 + 1) * box
+}
+
 let direction = "right";
+
+function drawFood(){
+    context.fillStyle = "red";
+    context.fillRect(food.x, food.y,box, box);
+}
 
 //Criar background
 function criarBG(){
@@ -40,8 +50,17 @@ function iniciarJogo(){
     if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
     if(snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
 
+    for(i=1 ; i < snake.length; i++){
+        if(snake[0].x == snake[i].x && snake[0].y == snake[i].y){
+            clearInterval(jogo);
+            alert("Game Over :( ");
+        }
+
+    }
+
     criarBG();
     criarCobrinha();
+    drawFood();
     
     let snakeX = snake[0].x;
     let snakeY = snake[0].y;
@@ -51,7 +70,15 @@ function iniciarJogo(){
     if(direction == "up") snakeY -= box;
     if(direction == "down") snakeY += box;
 
-    snake.pop();
+    if(snakeX != food.x || snakeY != food.y){
+        snake.pop();
+    }
+    else{
+        food.x = Math.floor(Math.random() * 15 + 1)* box;
+        food.y = Math.floor(Math.random() * 15 + 1) * box;
+    }
+
+    
 
     let newHead = {
         x: snakeX,
@@ -59,9 +86,14 @@ function iniciarJogo(){
     }
 
     snake.unshift(newHead);
+    
+    if(snake.length>3) setInterval(100);
+    
 }
 
 let jogo = setInterval(iniciarJogo,100);
+
+
 
 
 
